3be9a15294cc6a93f759641b0d81beb3aa62e4ba
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2022-07-23 11:07:05 -0400

    ex2

---
+++ JSFIP/src/8.3.1/main.mjs
@@ -0,0 +1,9 @@
+const ε = 0.0001;
+const строка = 'строка';
+let _tmp = 0;
+const $foo2 = true;
+
+console.log("ε="+ε);
+console.log("строка="+строка);
+console.log("_tmp="+_tmp);
+console.log("$foo2="+$foo2);
